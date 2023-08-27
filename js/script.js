ce3c2f5d59c22fbf4a12d95a23bd4c88bd6bd860
ubaid-syed <141223262+ubaid-syed@users.noreply.github.com>
2023-08-28 00:16:51 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// turn on function
+
+function bulbon(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","img/bulb-on.png");
+}
+
+function bulboff(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","img/bulb-off.png");
+}
